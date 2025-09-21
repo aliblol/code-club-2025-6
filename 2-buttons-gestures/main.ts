b0f63b5c2fef008890681d8b5912a0542b2d8423
Alice Brookes <72383278+aliblol@users.noreply.github.com>
2025-09-21 12:00:35 +0100

    example code

--- 2-buttons-gestures/main.ts
+++ 2-buttons-gestures/main.ts
@@ -1,4 +1,39 @@
-basic.showString("AHOY")
-basic.forever(function () {
-	
+/**
+ * Lesson 2: Buttons & Gestures Example Code
+ */
+// Clear screen after a short delay
+function clearAfterDelay () {
+    basic.pause(500)
+    basic.clearScreen()
+}
+// Button A pressed → show arrow up
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+    clearAfterDelay()
+})
+// A+B pressed → show heart
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Heart)
+    clearAfterDelay()
+})
+// Button B pressed → show arrow down
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # . # . #
+        . # # # .
+        . . # . .
+        `)
+    clearAfterDelay()
+})
+// Shake gesture → dice
+input.onGesture(Gesture.Shake, function () {
+    basic.showNumber(randint(1, 6))
 })
